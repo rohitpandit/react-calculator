ab0084137bcbd63391ff6fd801935b8a0bd2815d
Rohit Pandit <rohit0311@outlook.com>
2020-12-15 19:16:21 +0530

    skeleton

--- src/App.js
+++ src/App.js
@@ -1,9 +1,49 @@
-function App() {
-  return (
-    <div className="App">
-      <h1>calculator</h1>
-    </div>
-  );
+import React from  'react';
+import './app.css';
+
+
+class App extends React.Component{
+  render(){
+    return (
+
+      <div className="app">
+        <h1 className="result">Results</h1>
+        <div className="numbers">
+          <div className="row">
+            <button className="num">C</button>
+            <button className="num"> -- </button>
+            <button className="num">%</button>
+            <button className="num">/</button>
+          </div>
+          <div className="row">
+            <button className="num">7</button>
+            <button className="num">8</button>
+            <button className="num">9</button>
+            <button className="num">*</button>
+          </div>
+          <div className="row">
+            <button className="num">4</button>
+            <button className="num">5</button>
+            <button className="num">6</button>
+            <button className="num">-</button>
+          </div>
+          <div className="row">
+            <button className="num">1</button>
+            <button className="num">2</button>
+            <button className="num">3</button>
+            <button className="num">+</button>
+          </div>
+          <div className="row">
+            <button className="num">0</button>
+            <button className="num">0</button>
+            <button className="num">.</button>
+            <button className="num">=</button>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
+
 
 export default App;
