e73d86c7b6d74a6005e07b9a41003fee861b05de
Rohit Pandit <rohit0311@outlook.com>
2020-12-17 14:15:59 +0530

    done

--- src/App.js
+++ src/App.js
@@ -8,37 +8,36 @@ class App extends React.Component {
       nums: [], //different numbers as element of the array
       signs: [], //different symbols as element of the array
       last: 0, //the last number which is still unknown of its length
-      res: 0, //final result
+      flag: 0, //flag to avoid multiple clicks of number or signs
       show: '0', //the overall string which is viewed to the user
     };
   }
 
-  cal(){
+  cal() {
     let signs = this.state.signs;
     let nums = this.state.nums;
-    for(let i=0;i<4;i++){
-    
-      for(let j=0;j<signs.length;j++){
-        if(signs[j]==='/' && i===0){
-          let res = nums[j] / nums[j+1]; 
-          nums.splice(j,2,res);
-          signs.splice(j,1);
+    for (let i = 0; i < 4; i++) {
+      for (let j = 0; j < signs.length; j++) {
+        if (signs[j] === '/' && i === 0) {
+          let res = nums[j] / nums[j + 1];
+          nums.splice(j, 2, res);
+          signs.splice(j, 1);
         }
-        if(signs[j]==='*' && i===1){
-          let res = nums[j] * nums[j+1]; 
-          nums.splice(j,2,res);
-          signs.splice(j,1);
+        if (signs[j] === '*' && i === 1) {
+          let res = nums[j] * nums[j + 1];
+          nums.splice(j, 2, res);
+          signs.splice(j, 1);
         }
-        if(signs[j]==='+' && i===2){
-          let res = nums[j] + nums[j+1]; 
-          nums.splice(j,2,res);
-          signs.splice(j,1);
+        if (signs[j] === '+' && i === 2) {
+          let res = nums[j] + nums[j + 1];
+          nums.splice(j, 2, res);
+          signs.splice(j, 1);
         }
-        if(signs[j]==='-' && i===3){
-          let res = nums[j] - nums[j+1]; 
-          nums.splice(j,2,res);
-          signs.splice(j,1);
-        }        
+        if (signs[j] === '-' && i === 3) {
+          let res = nums[j] - nums[j + 1];
+          nums.splice(j, 2, res);
+          signs.splice(j, 1);
+        }
       }
     }
     return nums[0];
@@ -46,13 +45,17 @@ class App extends React.Component {
 
   handleButton(char) {
     if (char === 'C') {
-      this.setState({ last: 0, nums: [], signs: [], res: 0, show: "0" });
+      this.setState({ last: 0, nums: [], signs: [], flag: 0, show: '0' });
     } else if (char === '=') {
-      let nums = this.state.nums;
-      nums.push(this.state.last);
-      let res = 0;
-      res = this.cal();
-      this.setState({ show: res });
+      if (this.state.flag === 1) {
+        let nums = this.state.nums;
+        if (this.state.last !== 0) {
+          nums.push(this.state.last);
+        }
+        let res = 0;
+        res = this.cal();
+        this.setState({nums:[], signs:[], show: res, last: res, flag: 1 });
+      }
     } else if (
       char === 1 ||
       char === 2 ||
@@ -72,31 +75,31 @@ class App extends React.Component {
       } else {
         show += new String(char);
       }
-      this.setState({ last: last, show: show });
+      this.setState({ last: last, show: show, flag: 1 });
     } else {
-      let newNums = this.state.nums;
-      newNums.push(this.state.last);
-      let newSigns = this.state.signs;
-      newSigns.push(char);
-      let show = this.state.show;
-      show = show + char;
-      this.setState({
-        nums: newNums,
-        signs: newSigns,
-        last: 0,
-        show: show,
-      });
+      if (this.state.flag === 1) {
+        let newNums = this.state.nums;
+        newNums.push(this.state.last);
+        let newSigns = this.state.signs;
+        newSigns.push(char);
+        let show = this.state.show;
+        show = show + char;
+        this.setState({
+          nums: newNums,
+          signs: newSigns,
+          last: 0,
+          show: show,
+          flag: 0,
+        });
+      }
     }
   }
-
-  
 
   render() {
     return (
       <div className='app'>
         <h1 className='result'>{this.state.show}</h1>
         <div className='numbers'>
-          
           <div className='row'>
             <button onClick={(e) => this.handleButton(7, e)} className='num'>
               7
